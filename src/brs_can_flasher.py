ee533f263059641b8406b0fb85223c8df462670d
JanSpindler <JanSpindler>
2023-03-28 17:15:26 +0200

    script can read and write can messages

--- src/brs_can_flasher.py
+++ src/brs_can_flasher.py
@@ -1,16 +1,29 @@
 import can
+import time
+from tkinter import *
 
 
-def main():
+def main() -> None:
     print('General Kenobi')
     
     with can.Bus(channel=0, bitrate=1_000_000, interface='vector') as bus:
-        msg = can.Message(
+        tx_msg = can.Message(
             arbitration_id=0x200, 
             data=[0, 25, 0, 1, 3, 1, 4, 1], 
-            is_extended_id=False
+            is_extended_id=False,
+            is_rx=False
         )
-        bus.send(msg)
+
+        buffered_reader = can.BufferedReader()
+        printer = can.Printer()
+
+        notifier = can.Notifier(bus=bus, listeners=[buffered_reader, printer])
+
+        while True:
+            time.sleep(2.0)
+            bus.send(tx_msg)
+            print(tx_msg)
+
 
 
 if __name__ == '__main__':
