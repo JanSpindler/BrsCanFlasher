36f895f1c6a994437b278bc2885d991cae67ab68
JanSpindler <JanSpindler>
2023-03-28 13:41:14 +0200

    updated gen-cmake.py

--- gen-cmake.py
+++ gen-cmake.py
@@ -2,5 +2,6 @@ import os
 
 
 if __name__ == '__main__':
-	current_dir = 'C:/Users/jspin/CodeProjects/Test/BrsCanFlasher/'
+	current_dir = os.path.abspath(__file__)
+	print(current_dir)
 	os.system(f'cmake -DBRS_PROJ_PATH={current_dir} .')
